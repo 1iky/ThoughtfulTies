fc95bad1ca3974dde2ef49adfbad30e2a8daad7c
1iky <144616787+1iky@users.noreply.github.com>
2024-01-13 21:34:48 -0500

    Update ProfilePage.js

--- expoApp/src/screens/ProfilePage.js
+++ expoApp/src/screens/ProfilePage.js
@@ -8,58 +8,64 @@ const GalleryItem = ({ imageSource }) => (
 export default function ProfilePage() {
   const galleryData = [
     { id: '1', image: require('../../assets/images/user.jpeg') },
-    { id: '2', image: require('../../assets/images/user.jpeg') },
-    { id: '3', image: require('../../assets/images/user.jpeg') },
+    { id: '2', image: require('../../assets/images/cat.jpeg') },
+    { id: '3', image: require('../../assets/images/fish.jpeg') },
   ];
 
   const hobbies = ['Reading', 'Painting', 'Cooking', 'Cleaning', 'Bingo'];
 
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.profileContainer}>
-        <Text style={styles.appTitle}>Profile</Text>
-        <Image source={require('../../assets/images/user.jpeg')} style={styles.profilePic} />
-        <Text style={styles.name}>Bob Ross</Text>
-        <View style={styles.detailsContainer}>
-          <Text style={styles.detailsItem}>86</Text>
-          <Text style={styles.detailsItem}>         Male</Text>
-          <Text style={styles.detailsItem}>New York</Text>
-        </View>
-        <View style={styles.hobbiesContainer}>
-          {hobbies.map((hobby, index) => (
-            <View key={index} style={styles.hobbyBubble}>
-              <Text style={styles.hobbyText}>{hobby}</Text>
+    <View style={styles.container}>
+        <ScrollView style={styles.container}>
+        <View style={styles.profileContainer}>
+            <Text style={styles.appTitle}>Profile</Text>
+            <Image source={require('../../assets/images/userIcon.png')} style={styles.profilePic} />
+            
+            <View style={styles.nameContainer}>
+                <Text style={styles.name}>Bob Ross</Text>
             </View>
-          ))}
-        </View>
-      </View>
 
-      {/* Gallery Container */}
-      <Text style={styles.sectionTitle}>Gallery</Text>
-      <View style={styles.galleryContainer}>
-        <FlatList
-          data={galleryData}
-          keyExtractor={(item) => item.id}
-          horizontal
-          renderItem={({ item }) => <GalleryItem imageSource={item.image} />}
-        />
-      </View>
-
-      {/* Bottom Header */}
-      <View style={styles.bottomBubble}>
-        <View style={styles.bottomHeader}>
-          <TouchableOpacity style={styles.bottomButton}>
-            <Image source={require('../../assets/images/user.jpeg')} style={styles.bottomButtonImage} />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.bottomButton}>
-            <Image source={require('../../assets/images/user.jpeg')} style={styles.bottomButtonImage} />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.bottomButton}>
-            <Image source={require('../../assets/images/user.jpeg')} style={styles.bottomButtonImage} />
-          </TouchableOpacity>
+            <View style={styles.detailsContainer}>
+                <Text style={styles.detailsItem}>86</Text>
+                <Text style={styles.detailsItem}>Male</Text>
+                <Text style={styles.detailsItem}>New York</Text>
+            </View>
+            <View style={styles.hobbiesContainer}>
+            {hobbies.map((hobby, index) => (
+                <View key={index} style={styles.hobbyBubble}>
+                <Text style={styles.hobbyText}>{hobby}</Text>
+                </View>
+             ))}
+            </View>
         </View>
-      </View>
-    </ScrollView>
+
+        {/* Gallery Container */}
+        <Text style={styles.sectionTitle}>Gallery</Text>
+        <View style={styles.galleryContainer}>
+            <FlatList
+            data={galleryData}
+            keyExtractor={(item) => item.id}
+            horizontal
+            renderItem={({ item }) => <GalleryItem imageSource={item.image} />}
+            />
+        </View>
+
+        {/* Bottom Header */}
+        <View style={styles.bottomBubble}>
+            <View style={styles.bottomHeader}>
+            <TouchableOpacity style={styles.bottomButton}>
+                <Image source={require('../../assets/icons/messages.png')} style={styles.bottomButtonImage} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.bottomButton}>
+                <Image source={require('../../assets/icons/newLogo.png')} style={styles.bottomButtonImage} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.bottomButton}>
+                <Image source={require('../../assets/icons/profile.png')} style={styles.bottomButtonImage} />
+            </TouchableOpacity>
+            </View>
+        </View>
+        </ScrollView>
+    </View>
   );
 }
 
@@ -67,33 +73,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    padding: 8,
   },
-
   profileContainer: {
     alignItems: 'center',
-    paddingVertical: 20,
-  },
-
-  appTitle: {
-    fontSize: 17,
-    fontWeight: 'bold',
-    marginBottom: 10,
+    padding: 40,
     color: '#000',
   },
-
+  appTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 5,
+    marginBottom: 15,
+    color: '#000',
+  },
   profilePicContainer: {
     backgroundColor: '#8e70f5',
     borderRadius: 75,
     padding: 20, // Adjust padding as needed
     marginBottom: 10,
   },
-
   galleryContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
-    borderTopWidth: 5,
-    borderBottomWidth: 5,
+    borderTopWidth: 10,
+    borderBottomWidth: 10,
     borderBottomColor: '#Dcd7ea',
     borderTopColor: '#Dcd7ea',
     paddingVertical: 1,
@@ -102,8 +107,6 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     padding: 10,
   },
-
-
   bottomBubble: {
     flexDirection: 'row',
     justifyContent: 'space-around',
@@ -113,50 +116,33 @@ const styles = StyleSheet.create({
     paddingVertical: 1,
     backgroundColor: '#8e70f5',
     borderRadius: 50,
-  },
-
-  appTitle: {
-    fontSize: 17,
-    fontWeight: 'bold',
-    marginBottom: 20,
-    textAlign: 'center',
-    color: '#000',
-  },
-
-  profileContainer: {
-    alignItems: 'center',
-    paddingVertical: 20,
+    marginTop: 20,
   },
   profilePic: {
     width: 150,
     height: 150,
-    borderRadius: 75,
-    marginBottom: 10,
   },
   name: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 5,
-    alignItems: 'center',
+    textAlign: 'center',
   },
   detailsContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    width: '80%', 
-    marginBottom: 25,
-    marginTop: 25
+    width: '75%', 
+    marginBottom: 10,
   },
   detailsItem: {
     fontSize: 17,
     color: '#777',
+    textAlign: 'center',
   },
-
   hobbiesContainer: {
     flexDirection: 'row',
     justifyContent: 'center',
     flexWrap: 'wrap',
-    marginBottom: 5,
   },
   hobbyBubble: {
     backgroundColor: '#8e70f5',
@@ -172,41 +158,49 @@ const styles = StyleSheet.create({
     fontWeight: 'bold', 
   },
   sectionContainer: {
-    padding: 20,
+    padding: 30,
   },
-
   sectionTitle: {
     fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 10,
+    marginBottom: 5,
+    marginLeft: 5,
   },
   galleryImage: {
     width: 150,
     height: 150,
     marginRight: 10,
-    borderRadius: 20,
+    borderRadius: 25,
   },
-
   bottomButtonImage: {
-    width: 65,
-    height: 65,
+    width: 45,
+    height: 45,
   },
-
   bottomHeader: {
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
     flex: 1, 
-    margin: 5
-    
   },
-
   bottomButton: {
-    paddingVertical: 7, 
-    paddingHorizontal: 7,
+    paddingVertical: 8, 
+    paddingHorizontal: 8,
     flex: 1,
     alignItems: 'center',
     paddingLeft: 5
+  },
+  nameContainer: {
+    color: '#fff',
+    padding: 20,
+    borderRadius: 25,
+    width: 250,
+    alignItems: 'center',
+  },
+  purpleBgContainer: {
+    flex: 1,
+    color: '#DCD7EA',
+    padding: 20,
+    borderRadius: 20,
   },
   
 });
